fed8dd102f65c6cd9e6f97cd3015c65af09fff4a
Arthigasri <arthi1461997@gmail.com>
2017-07-05 10:28:42 +0530

    Create Main2.java

---
+++ Main2.java
@@ -0,0 +1,22 @@
+# art1
+import java.io.*;
+ import java.util.*;
+ public class Main2
+{
+  public static void main(String args[]) throws IOException
+  {
+    int a;  
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter the number");
+    a=Integer.parseInt(br.readLine());
+    if(a%2==0)
+    {
+      System.out.println("Enter an even number");
+      }
+      else
+      {
+        System.out.println("Enter an odd number");
+        }
+        }
+        }
+        
